17067dde7013f1058ff687ab0debfa849209fab7
NiquetheLegendary <141907068+NiquetheLegendary@users.noreply.github.com>
2024-04-03 17:03:47 +0200

    Add files via upload

--- project1/js/image-gallery.js
+++ project1/js/image-gallery.js
@@ -1,24 +1,50 @@
 // JavaScript Document
-let largeimage=document.querySelector("#largeimage");
-let caption=document.querySelector("#caption");
-let thumbnails=document.querySelector("#thumbnails").children;
 
 
-// selectimage function for handling click events
-function selectimage (galleryimage){
-    largeimage.src = galleryimage.src;
-    for (let i = 0; 1 < thumbnails.length; i++) thumbnails[i].classList.remove("thumbup");
-    galleryimage.classlist.add("thumbup")
-    console.log(this);
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let img5 = document.getElementById("img5");
+
+let imageCaption = document.getElementById("caption");
+
+let lrgImg = document.getElementById("largeImage");
+
+function changeImg(){
+    var imgSrc = this.getAttribute("src");
+    var imgCaption = this.getAttribute("alt");
+    lrgImg.setAttribute("src", imgSrc);
+    imageCaption.innerHTML = imgCaption;
+};
+
+function showCaption(){
+    var imgCaption = this.getAttribute("alt");
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "newDiv");
+    newDiv.innerHTML = imgCaption;  
+    this.after(newDiv);
+};
+
+function hideCaption(){
+    var newDiv = document.querySelector(".newDiv");
+    newDiv.remove();
+};
+
+function addBorder(){
+    for (i = 1; i < 6; i++){
+        var currentImg = document.getElementById("img" + i);
+        currentImg.setAttribute("class", "thumb");
+    } 
+    this.setAttribute("class", "active-image");
+};
+
+for (i = 1; i < 6; i++){
+    var currentImg = document.getElementById("img" + i);
+    currentImg.addEventListener("click", changeImg);
+    currentImg.addEventListener("click", addBorder);
+    currentImg.addEventListener("mouseover", showCaption);
+    currentImg.addEventListener("mouseout", hideCaption);
 }
+    
 
-// hoverimage function for handling hover events
-function hoverimage (galleryimage){
-    caption.innerHTML=galleryimage.alt;
-    console.log(this); 
-}
-
-//creates border
-function mark(ID) { 
-    document.getElementById(this).style.border = "3px solid white";
-  }
